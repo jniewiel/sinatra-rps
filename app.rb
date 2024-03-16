13efc89fa097b68751fad1226bd5e8d8bb04314f
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-03-16 02:28:40 +0000

    created layout, made settings changes

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
-require 'sinatra'
+# app.rb
+# `bundle install`
+# run with `ruby dice.rb`
+
+require "sinatra"
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get('/') do
   "
