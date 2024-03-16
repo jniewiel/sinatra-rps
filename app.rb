3508480da8cccede6cefcb37af3edba81e90aa6b
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-03-16 04:22:05 +0000

    Created rock.erb

--- app.rb
+++ app.rb
@@ -12,6 +12,32 @@ use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
+# ------------------------------------------------------------------ #
+
 get('/') do
   erb(:main)
 end
+
+# ------------------------------------------------------------------ #
+
+get('/rock') do
+  options = ["rock", "paper", "scissors"]
+  @computer = options.sample
+  @player = "rock"
+
+  erb(:rock)
+end
+
+# ------------------------------------------------------------------ #
+
+get('/paper') do
+  erb(:paper)
+end
+
+# ------------------------------------------------------------------ #
+
+get('/scissors') do
+  erb(:scissors)
+end
+
+# ------------------------------------------------------------------ #
